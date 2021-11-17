f8f78a7a943c51c1a6d2de2aff9d9bc5b78370c1
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-11-16 21:56:38 -0500

    footer cleanup

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
         <section className="my-5">
             <a href='https://github.com/stephgeorge22'><img src="https://img.icons8.com/material-outlined/24/000000/github.png" alt="GitHub" /> </a>
             <a href='https://www.linkedin.com/in/stephanie-george-eit-env-sp/'><img src="https://img.icons8.com/ios-glyphs/30/000000/linkedin.png" alt="LinkedIn" /> </a>
-            <a href='https://stackoverflow.com/users/15562159/stephgeorge'><img src="https://img.icons8.com/windows/32/000000/stackoverflow.png" alt="LinkedIn" /> </a>
+            {/* <a href='https://stackoverflow.com/users/15562159/stephgeorge'><img src="https://img.icons8.com/windows/32/000000/stackoverflow.png" alt="LinkedIn" /> </a> */}
 
             
         </section>
