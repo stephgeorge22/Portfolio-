d3ecd70699dc5748e20e0132cc2f6e702d21d4fb
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-12-06 22:22:41 -0500

    update about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
         <section className="my-5">
             <h1 id="about">About Me</h1>
             <img src={aboutImage} className="my-2" style={{ width: "8%" }} alt="about me" />
-            <p>A Full Stack Engineer pivoting to building the digital world from the physical world. With five years of professional experience and an Engineering degree, I am highly analytical, client/customer oriented and a true problem solver at heart. I recently earned a certificate in full stack development from the Rutgers Bootcamp, with a focus on JavaScript, bootstrap, node.js, express.js, and sequel. Thanks for stopping by my portfolio, feel free to browse to see how I’ve utilized my skills to write algorithms and create applications. </p>
+            <p>A Full Stack Engineer pivoting to building the digital world from the physical world (as a current Civil Engineer). With five years of professional experience and an Engineering degree, I am highly analytical, client/customer oriented and a true problem solver at heart. I’m excited to pursue my passion of building applications to improve the daily life of people while delivering an exceptional user-experience. I recently earned a certificate in full stack development from the Rutgers Bootcamp, with a focus in JavaScript, React, Node.js and Express.js. As an eager learner with a knack for facilitating collaboration, I’m looking forward to joining your team to continue to develop innovation and become a valuable asset! Keep broswing my portfolio to see how I’ve utilized my skills to write algorithms and create applications.  </p>
         </section>
     );
 }
